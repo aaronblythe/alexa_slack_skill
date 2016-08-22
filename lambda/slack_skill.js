b0e8878e19c22c2278fe3126994afc400aee07e7
Aaron Blythe <ablythe@hearst.com>
2016-08-22 02:08:39 -0500

    latest working code

--- lambda/slack_skill.js
+++ lambda/slack_skill.js
@@ -1,28 +1,44 @@
 
 var https = require('https');
+var pagerduty_auth = "Token token=<your pagerduty token>";
+var pagerduty_from = "<your email>";
+var match = false;
+
 var options = {
   host: 'hooks.slack.com',
   port: 443,
-  path: '/services/<your web hook>',
+  path: '/services/T0P8KN51P/B1Z8A6L78/rj5hhXhTr1tnyBXiz8Ag4uDO',
   method: 'POST'
 };
 
 var pagerduty_get_options = {
-  host: 'api.pagerduty.com',
-  port: 443,
-  path: '/incidents',
-  method: 'GET'
+  hostname: 'api.pagerduty.com',
+  port: '443',
+  path: '/incidents?limit=50',
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.pagerduty+json;version=2',
+    'From': pagerduty_from,
+    'Authorization': pagerduty_auth
+  }
 };
 
-var pagerduty_put_options = {
-  host: 'api.pagerduty.com',
-  port: 443,
+var pd_update_options = {
+  hostname: 'api.pagerduty.com',
+  port: '443',
   path: '/incidents',
-  method: 'PUT'
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.pagerduty+json;version=2',
+    'From': pagerduty_from,
+    'Authorization': pagerduty_auth
+  }
 };
 
-var pagerduty_auth = "Token token=<your token>";
-var pagerduty_from = "<your email>";
+
+
 
 /**
  * This sample shows how to create a simple Lambda function for handling speechlet requests.
@@ -114,6 +130,10 @@ function onIntent(intentRequest, session, callback) {
         console.log("PagerMeIntent");
         sendPagerMe(intent, session, callback);
     }
+    else if ("ReactIntent" === intentName) {
+        console.log("ReactIntent");
+        sendReaction(intent, session, callback);
+    }
     else if ("NewRelicIntent" === intentName) {
         console.log("NewRelicIntent");
         sendNewRelicCommand(intent, session, callback);
@@ -126,8 +146,8 @@ function onIntent(intentRequest, session, callback) {
         console.log("PagerDutyAckIntent");
         ackPagerDutyIncident(intent, session, callback);
     } 
-    else if ("PagerDutyResolveIntent" === intentName) {
-        console.log("PagerDutyResolveIntent");
+    else if ("PDResolveIntent" === intentName) {
+        console.log("PDResolveIntent");
         resolvePagerDutyIncident(intent, session, callback);
     } 
     else if ("WhatsMyMessageIntent" === intentName) {
@@ -272,6 +292,43 @@ function thatsThePlan(intent, session, callback) {
     
 }
 
+function sendReaction(intent, session, callback) {
+    var cardTitle = intent.name;
+    var messageSlot = intent.slots.ReactMessage;
+    var repromptText = "";
+    var sessionAttributes = {};
+    var shouldEndSession = true;
+    var speechOutput = "";
+    if (messageSlot) {
+        message = messageSlot.value;
+        console.log("Message slot contains: " + message + ".");
+        sessionAttributes = createMessageAttributes(message);
+        speechOutput = message + "reaction sent.";
+        repromptText = message + "reaction sent.";
+        var req = https.request(options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', function (chunk) {
+                callback(sessionAttributes, 
+                buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+            });
+        });
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+            context.fail(e);
+        });
+        req.write('{"channel": "#bu_bot_test", "username": "Aaron\'s Amazon Echo", "text": "atbot react ' + message + '", "icon_emoji": ":echo:"}');
+        req.end();
+    } else {
+        speechOutput = "I didn't hear your message clearly, please try again";
+        repromptText = "I didn't hear your message clearly, you can give me your "
+                + "message by saying, my message is...";
+    callback(sessionAttributes, 
+             buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+    }
+    
+    
+}
+
 function sendPagerMe(intent, session, callback) {
     var cardTitle = intent.name;
     var messageSlot = intent.slots.PagerMeMessage;
@@ -321,9 +378,8 @@ function sendNewRelicCommand(intent, session, callback) {
         message = messageSlot.value;
         console.log("Message slot contains: " + message + ".");
         sessionAttributes = createMessageAttributes(message);
-        speechOutput = "Your message has been sent. You can ask me to repeat it by saying, "
-                + "what's my message?";
-        repromptText = "You can ask me to repeat your message by saying, what's my message?";
+        speechOutput = "The latest server information should now show in the Slack Room.";
+        repromptText = "The latest server information should now show in the Slack Room.";
         var req = https.request(options, function(res) {
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
@@ -359,14 +415,9 @@ function createPagerDutyIncident(intent, session, callback) {
         message = messageSlot.value;
         console.log("Message slot contains: " + message + ".");
         sessionAttributes = createMessageAttributes(message);
-        speechOutput = "Your message has been sent. You can ask me to repeat it by saying, "
-                + "what's my message?";
-        repromptText = "You can ask me to repeat your message by saying, what's my message?";
+        speechOutput = "I have told the Slack room to create a new incident for " + message;
+        repromptText = "I have told PagerDuty to create a new incident for " + message;
         var req = https.request(options, function(res) {
-            setHeader('Content-Type', 'application/json');
-            setHeader('Accept', 'application/vnd.pagerduty+json;version=2');
-            setHeader('From', pagerduty_form);
-            setHeader('Authorization', pagerduty_auth);
             res.setEncoding('utf8');
             res.on('data', function (chunk) {
                 callback(sessionAttributes, 
@@ -399,28 +450,91 @@ function ackPagerDutyIncident(intent, session, callback) {
     var shouldEndSession = false;
     var speechOutput = "";
     if (messageSlot) {
-        message = messageSlot.value;
-        console.log("Message slot contains: " + message + ".");
-        sessionAttributes = createMessageAttributes(message);
-        speechOutput = "Your message has been sent. You can ask me to repeat it by saying, "
-                + "what's my message?";
-        repromptText = "You can ask me to repeat your message by saying, what's my message?";
+        number = messageSlot.value;
+        console.log("Message slot contains: " + number + ".");
+        sessionAttributes = createMessageAttributes(number);
+        speechOutput = "I have told PagerDuty to acknowledge incident number" + number;
+        repromptText = "I have told PagerDuty to acknowledge incident number" + number;
         var req = https.request(pagerduty_get_options, function(res) {
-            setHeader('Content-Type', 'application/json');
-            setHeader('Accept', 'application/vnd.pagerduty+json;version=2');
-            setHeader('From', pagerduty_from);
-            setHeader('Authorization', pagerduty_auth);
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
+          var info = '';
+          res.setEncoding('utf8');
+          res.on('data', (chunk) => {
+            info += chunk;
+          });
+          res.on('end', () => {
+            content = JSON.parse(info);
+            if (res.statusCode != 200){
+              console.log('pagerduty get issues failed')
+              console.log('statusCode:', res.statusCode);
+              console.log(content);
+            }
+            //console.log(content);
+            ref = content.incidents;
+            if (number == "latest") {
+              number = 0;
+              for (j = 0, len = content.incidents.length; j < len; j++) {
+                if (content.incidents[j].incident_number > number){
+                  number = content.incidents[j].incident_number;
+                }
+              }
+              console.log("latest was sent in using: " + number);
+            }
+      
+            for (i = 0, len = content.incidents.length; i < len; i++) {
+              
+              if (content.incidents[i].incident_number == number) {
+                match = true;
+                console.log("************found it***********");
+                console.log(content.incidents[i].id);
+                // Acknowledge the incident that was matched
+                var update_req = https.request(pd_update_options, function(update_res) {
+                  var update_info = '';
+                  update_res.setEncoding('utf8');
+                  update_res.on('data', (chunk) => {
+                    update_info += chunk;
+
+                  });
+                  update_res.on('end', () => {
+                    update_content = JSON.parse(update_info);
+                    if (update_res.statusCode != 200){
+                      console.log('pagerduty ack issues failed')
+                      console.log('statusCode:', update_res.statusCode);
+                      console.log(update_content);
+                    }
+                    console.log(update_content);
+
+                  });
+                });
+                var putdata = JSON.stringify({
+                  "incidents": [{
+                    "id" : content.incidents[i].id,
+                    "type" : "incident",
+                    "status" : "acknowledged"
+                  }]
+                });
+                console.log(putdata);
+                update_req.write(putdata);
+                update_req.on('error', (e) => {
+                  console.error(e);
+                  context.fail(e);
+                });
+                //feel like this should go in the on update_res.on('end'... but is timing out.
+                speechOutput = "I have told Pager Duty to acknowledge incident number" + number;
+                repromptText = "I have told Pager Duty to acknowledge incident number" + number;
                 callback(sessionAttributes, 
-                buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-            });
+                      buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+                update_req.end();
+              }
+            }
+            //if (match == false){
+            // console.log("Did not match the incident: " + number);
+            //}
+          });
         });
-        req.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-            context.fail(e);
+        req.on('error', (e) => {
+          console.error(e);
+          context.fail(e);
         });
-        req.write('{"channel": "#bu_bot_test", "username": "Aaron\'s Amazon Echo", "text": "bubot pager ack ' + message + '", "icon_emoji": ":echo:"}');
         req.end();
     } else {
         speechOutput = "I didn't hear your message clearly, please try again";
@@ -441,25 +555,91 @@ function resolvePagerDutyIncident(intent, session, callback) {
     var shouldEndSession = true;
     var speechOutput = "";
     if (messageSlot) {
-        message = messageSlot.value;
-        console.log("Message slot contains: " + message + ".");
-        sessionAttributes = createMessageAttributes(message);
-        speechOutput = "Your message has been sent. You can ask me to repeat it by saying, "
-                + "what's my message?";
-        repromptText = "You can ask me to repeat your message by saying, what's my message?";
-        var req = https.request(options, function(res) {
-            res.setEncoding('utf8');
-            res.on('data', function (chunk) {
-                callback(sessionAttributes, 
-                buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
-            });
+        number = messageSlot.value;
+        console.log("Message slot contains: " + number + ".");
+        sessionAttributes = createMessageAttributes(number);
+        speechOutput = "I have told PagerDuty to resolve incident number" + number;
+        repromptText = "I have told PagerDuty to resolve incident number" + number;
+        var req = https.request(pagerduty_get_options, function(res) {
+          var info = '';
+          res.setEncoding('utf8');
+          res.on('data', (chunk) => {
+            info += chunk;
+          });
+          res.on('end', () => {
+            content = JSON.parse(info);
+            if (res.statusCode != 200){
+              console.log('pagerduty get issues failed')
+              console.log('statusCode:', res.statusCode);
+              console.log(content);
+            }
+            ref = content.incidents;
+            if (number == "latest") {
+              number = 0;
+              for (j = 0, len = content.incidents.length; j < len; j++) {
+                if (content.incidents[j].incident_number > number){
+                  number = content.incidents[j].incident_number;
+                }
+              }
+              console.log("latest was sent in using: " + number);
+            }
+      
+            for (i = 0, len = content.incidents.length; i < len; i++) {
+              
+              if (content.incidents[i].incident_number == number) {
+                match = true;
+                console.log("************found it***********");
+                console.log(content.incidents[i].id);
+                // Resolve the incident that was matched
+                var update_req = https.request(pd_update_options, function(update_res) {
+                  var update_info = '';
+                  update_res.setEncoding('utf8');
+                  update_res.on('data', (chunk) => {
+                    update_info += chunk;
+                  });
+                  update_res.on('end', () => {
+                    update_content = JSON.parse(update_info);
+                    if (update_res.statusCode != 200){
+                      console.log('pagerduty resolve issues failed')
+                      console.log('statusCode:', update_res.statusCode);
+                      console.log(update_content);
+                    }
+                    console.log('statusCode:', update_res.statusCode);
+                    console.log(update_content);
+                  });
+                });
+                var putdata = JSON.stringify({
+                  "incidents": [{
+                    "id" : content.incidents[i].id,
+                    "type" : "incident",
+                    "status" : "resolved"
+                  }]
+                });
+                console.log(putdata);
+                update_req.write(putdata);
+                update_req.on('error', (e) => {
+                  console.error(e);
+                  context.fail(e);
+                });
+                //feel like this should go in the on update_res.on('end'... but is timing out.
+                repromptText = "I have told Pager Duty to resolve incident number" + number;
+                speechOutput = "I have told Pager Duty to resolve incident number" + number;
+                callback(sessionAttributes, buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+                
+                update_req.end();
+              }
+            }
+            if (match == false){
+              console.log("Did not match the incident: " + number);
+            }
+          });
         });
-        req.on('error', function(e) {
-            console.log('problem with request: ' + e.message);
-            context.fail(e);
+        req.on('error', (e) => {
+          console.error(e);
+          context.fail(e);
         });
-        req.write('{"channel": "#bu_bot_test", "username": "Aaron\'s Amazon Echo", "text": "bubot pager resolve ' + message + '", "icon_emoji": ":echo:"}');
         req.end();
+ 
     } else {
         speechOutput = "I didn't hear your message clearly, please try again";
         repromptText = "I didn't hear your message clearly, you can give me your "
